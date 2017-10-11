e49c231c28789fce226b478eaac7f8ce58e8472c
admin <admin@Lab-1-Instructor.local>
2017-10-10 21:18:07 -0500

    Completed Swatch App

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
+$('#generateBtn').click(function(){
 
-$(document).click(function(){
-	$.getJSON("js/colors.json", function(result,status){
-		console.log(result);
+	$("#swatchBoard").html('');	
+
+	var randomSwatch = Math.floor( Math.random() * 3 );
+
+	$.getJSON("js/colors.json", function(result){
+		
+		for(var i = 0; i<result[randomSwatch].swatch.length; i++){
+			var hex=String(result[randomSwatch].swatch[i].code.hex);
+			var color=String(result[randomSwatch].swatch[i].color);
+			
+			console.log(hex + " " + color);
+
+			$("#swatchBoard").append('<div class="swatch" style="background-color: ' + hex + ';"><h1>' + color + '</h1>' + '<div class="hidden"><ul><li>' + String(result[randomSwatch].swatch[0].color) + '</li><li>' + String(result[randomSwatch].swatch[i].category) + '</li><li>' + String(result[randomSwatch].swatch[i].type) + '</li><li>' + String(result[randomSwatch].swatch[i].code.hex) + '</li></ul></div>' + '</div>');
+		}
+		$('.swatch').each(function(){
+			$(this).click(function(){
+				$(this).children('.hidden').css("opacity","1");
+			});
+		});
 	});
+
 });
